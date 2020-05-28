ae482c07c2ab9f90c9881045ad827b1c740980b8
sebas_z <zanabria.s@pucp.pe>
2020-05-27 19:45:12 -0500

    Self join association created

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,6 @@ class Game < ApplicationRecord
     has_many :companies, through: :involved_companies
     has_and_belongs_to_many :platforms
     has_and_belongs_to_many :genres
+    has_many :expansions, class_name: "Game", foreign_key: "parent_id"
+    belongs_to :parent, class_name: "Game", optional: true
 end
